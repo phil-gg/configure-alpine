e903ae9ae2671bff8da3afa9ce3fc33e0912c2ed
Philip Antrobus <45383744+phil-gg@users.noreply.github.com>
2023-10-17 14:51:49 +1000

    Update script-updater.sh

--- script-updater.sh
+++ script-updater.sh
@@ -22,9 +22,9 @@ greenbold=$(printf '\033[92;1m')
 cyanbold=$(printf '\033[96;1m')
 bluebold=$(printf '\033[94;1m')
 
-# 
+# Show run time on screen
 
-echo -e "\n${bluebold}Update run at:${normal}"
+echo -e "\n${bluebold}Update run at${normal}"
 echo -e "  ${runtime}"
 
 # Network test
@@ -150,6 +150,6 @@ fi
 
 # Now run configure-alpine.sh
 
-echo -e "\n${bluebold}Run ‘configure-alpine.sh’${normal}"
+echo -e "\n${bluebold}Now running ‘configure-alpine.sh’${normal}"
 
 busybox sh configure-alpine.sh
