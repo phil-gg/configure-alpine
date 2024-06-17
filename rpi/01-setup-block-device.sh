72fc348f15c300af82daa8bed541c06c18cb7230
Philip Antrobus <45383744+phil-gg@users.noreply.github.com>
2024-06-17 19:14:00 +1000

    Update 01-setup-block-device.sh

--- rpi/01-setup-block-device.sh
+++ rpi/01-setup-block-device.sh
@@ -10,6 +10,13 @@
 # https://google.github.io/styleguide/shellguide.html
 ################################################################################
 
+# Exit script if executed without root privileges
+
+if (whoami != root)
+  then echo "This script requires root privileges to execute.  Exiting"
+  exit
+  else
+
 # Set variables
 
 block_device="/dev/nvme0n1"
@@ -21,7 +28,28 @@ bluebold=$(printf '\033[94;1m')
 
 # Warning 10 second countdown
 
-# TO-DO = Add countdown here
+echo -e "\n${redbold}WARNING: About to erase ${normal}${block_device}"
+echo -ne "10"
+sleep 1
+echo -ne "\033[2K 9"
+sleep 1
+echo -ne "\033[2K 8"
+sleep 1
+echo -ne "\033[2K 7"
+sleep 1
+echo -ne "\033[2K 6"
+sleep 1
+echo -ne "\033[2K 5"
+sleep 1
+echo -ne "\033[2K 4"
+sleep 1
+echo -ne "\033[2K 3"
+sleep 1
+echo -ne "\033[2K 2"
+sleep 1
+echo -ne "\033[2K 1"
+sleep 1
+echo -e "\033[2K 0"
 
 # Partitioning scheme for block device:
 # (1) 1G (Gibibyte) "Bootfs" right at start (FAT32).
@@ -29,3 +57,7 @@ bluebold=$(printf '\033[94;1m')
 # (3) "Rootfs" +129M after Swapfs and ending -129M from end of device.
 
 # 
+
+# Close the root privileges if-then
+
+fi
