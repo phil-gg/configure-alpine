a78c91ea66371a71cee0a7fc89e4ec8b04338795
Philip Antrobus <45383744+phil-gg@users.noreply.github.com>
2024-06-17 17:58:07 +1000

    Create 01-setup-block-device.sh

---
+++ rpi/01-setup-block-device.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+################################################################################
+# Block device set-up script.
+#
+# Choice of shebang allows for execution by busybox.
+# (i.e. run this file with `busybox sh script-updater.sh`)
+#
+# This shell script attempts to comply with:
+# https://google.github.io/styleguide/shellguide.html
+################################################################################
+
+# Set variables
+
+block_device="/dev/nvme0n1"
+normal=$(printf '\033[0m')
+redbold=$(printf '\033[91;1m')
+greenbold=$(printf '\033[92;1m')
+cyanbold=$(printf '\033[96;1m')
+bluebold=$(printf '\033[94;1m')
+
+# Partitioning scheme for block device:
+# (1) 1G (Gibibyte) "Bootfs" right at start (FAT32).
+# (2) 10G (Gibibyte) "Swapfs" +129M (Mebibytes) after Bootfs.
+# (3) "Rootfs" +129M after Swapfs and ending -129M from end of device.
+
+# 
