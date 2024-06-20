353ca72bb5d59ab13388d63fdf7c831c5df81d09
Philip Antrobus <45383744+phil-gg@users.noreply.github.com>
2024-06-20 11:47:27 +1000

    Update 02-alpine-onto-blk-dev.sh

--- rpi/02-alpine-onto-blk-dev.sh
+++ rpi/02-alpine-onto-blk-dev.sh
@@ -86,6 +86,18 @@ if [ $? -eq 0 ];
     printf "%b\n" "${redbold}  ERROR: sha256 mismatch${normal}"
     exit 102
 fi
+printf "%b\n" "\n${bluebold}Compare Alpine download to checksum${normal}"
+echo "${sha512}" | sha512sum -c 1> /dev/null
+# shellcheck disable=SC2181  # Okay with doing this check indirectly
+if [ $? -eq 0 ];
+  then
+    printf "%b\n" "${greenbold}  SUCCESS: sha512 matches${normal}"
+  else
+    printf "%b\n" "${redbold}  ERROR: sha512 mismatch${normal}"
+    exit 102
+fi
+
+
 
 # Put additions here
 
